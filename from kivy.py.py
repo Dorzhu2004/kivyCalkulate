48dcfabfc558c2b1690e346f518b9edf67746da8
Dorzhu2004 <144753979+Dorzhu2004@users.noreply.github.com>
2024-02-27 12:18:19 +0300

    Add files via upload

---
+++ from kivy.py.py
@@ -0,0 +1,22 @@
+def calculator():
+    num1 = float(input("Введите первое число: "))
+    operator = input("Введите оператор (+, -, *, /): ")
+    num2 = float(input("Введите второе число: "))
+
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            result = "Деление на ноль невозможно"
+    else:
+        result = "Некорректный оператор"
+
+    print("Результат:", result)
+
+calculator()
